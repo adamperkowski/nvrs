2c4d6b1939dd300e219c6dc7f0e67c2276581671
Adam Perkowski <adas1per@protonmail.com>
2025-01-02 18:53:47 +0100

    S Q U A S H

--- src/tui/main.rs
+++ src/tui/main.rs
@@ -1,1 +1,38 @@
+use anyhow::Result;
+mod state;
 
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut app = state::App::new()
+        .await
+        .map_err(|e| {
+            e.pretty();
+            std::process::exit(1)
+        })
+        .unwrap();
+    let mut terminal = ratatui::init();
+
+    while app.is_running {
+        terminal.draw(|f| app.draw(f))?;
+
+        if app.effect_is_running() {
+            continue;
+        }
+
+        if app.is_syncing {
+            app.sync().await?;
+        } else {
+            app.handle_events()?;
+        }
+    }
+
+    app.effect_reverse();
+
+    while app.effect_is_running() {
+        terminal.draw(|f| app.draw(f))?;
+    }
+
+    ratatui::restore();
+
+    Ok(())
+}
